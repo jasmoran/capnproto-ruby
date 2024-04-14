8fed8d39fbefc34924eb02c46717ca2815d1c204
Jack Moran <jack@earth.co.nz>
2024-04-14 19:30:34 +1200

    Implement CodeGeneratorRequest.RequestedFile

--- schema.capnp.rb
+++ schema.capnp.rb
@@ -26,9 +26,55 @@ module Schema
     sig { returns(CapnpVersion) }
     def capnpVersion = CapnpVersion.new(@segment, pointer_offset(2))
 
+    sig { returns(T::Array[Integer]) }
+    def requestedFiles
+      pointer = CapnProto::ListPointer.new(@segment, pointer_offset(1))
+      (0...pointer.length).map do |ix|
+        pointer.get(ix, :u64)
+      end
+    end
+
     sig { returns(T::Hash[Symbol, T.untyped]) }
     def to_h = {
       capnpVersion: capnpVersion.to_h,
+      requestedFiles: requestedFiles,
     }
+
+    class RequestedFile < CapnProto::StructPointer
+      sig { returns(Integer) }
+      def id = read_integer(0, :u64)
+
+      sig { returns(String) }
+      def filename = CapnProto::StringPointer.new(@segment, pointer_offset(0)).value
+
+      # sig { returns(T::Array[Import]) }
+      # def imports
+      #   pointer = CapnProto::ListPointer.new(@segment, pointer_offset(1))
+      #   (0...pointer.length).map do |ix|
+      #     Import.new(@segment, )
+      #   end
+      # end
+
+      sig { returns(T::Hash[Symbol, T.untyped]) }
+      def to_h = {
+        id: id,
+        filename: filename,
+        imports: nil,
+      }
+
+      class Import < CapnProto::StructPointer
+        sig { returns(Integer) }
+        def id = read_integer(0, :u64)
+
+        sig { returns(String) }
+        def name = CapnProto::StringPointer.new(@segment, pointer_offset(0)).value
+
+        sig { returns(T::Hash[Symbol, T.untyped]) }
+        def to_h = {
+          id: id,
+          name: name,
+        }
+      end
+    end
   end
 end
