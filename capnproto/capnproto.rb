e1a730e1a86375ffd3dea05345fb27c80d3849e5
Jack Moran <jack@earth.co.nz>
2024-04-15 15:24:27 +1200

    Remove Segment class

--- capnproto/capnproto.rb
+++ capnproto/capnproto.rb
@@ -11,21 +11,4 @@ module CapnProto
   def self.assert(&block)
     Kernel.raise 'Assertion failed' unless yield
   end
-
-  class Segment
-    extend T::Sig
-
-    sig { params(message: Message, offset: Integer, size: Integer).void }
-    def initialize(message, offset, size)
-      @message = message
-      @offset = offset
-      @size = size
-    end
-
-    sig { params(offset: Integer, size: Integer, encoding: Encoding).returns(::String) }
-    def read_string(offset, size, encoding) = ""
-
-    sig { params(offset: Integer, type: Symbol).returns(Integer) }
-    def read_integer(offset, type) = 0
-  end
 end
