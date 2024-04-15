3b5e294b1f47a278b7b9785ebe228d27ebdd0af1
Jack Moran <jack@earth.co.nz>
2024-04-15 15:23:53 +1200

    Remove *Pointer classes

--- capnproto/list.rb
+++ capnproto/list.rb
@@ -63,65 +63,13 @@ class CapnProto::String < CapnProto::List
   def value = @data.read_string(0, @length - 1, Encoding::UTF_8)
 end
 
-class CapnProto::ListPointer
-  extend T::Sig
-
-  LIST_ELEMENT_SIZES = T.let({
-    2 => 1,
-    3 => 2,
-    4 => 4,
-    5 => 8,
-  }.freeze, T::Hash[Integer, Integer])
-
-  sig { params(segment: CapnProto::Segment, offset: Integer).void }
-  def initialize(segment, offset)
-    @segment = segment
-    @offset = offset
-
-    # Check the type of the pointer
-    offset_part = @segment.read_integer(@offset, :s32)
-    CapnProto::assert { offset_part & 0b11 == 1 }
-
-    # Extract offset of first element
-    offset_from_pointer = (offset_part >> 2) * CapnProto::WORD_SIZE
-    @data_offset = T.let(@offset + offset_from_pointer + CapnProto::WORD_SIZE, Integer)
-
-    # Extract type of list elements
-    size_part = @segment.read_integer(@offset + 4, :u32)
-    element_type = size_part & 0b111
-    element_size = LIST_ELEMENT_SIZES[element_type]
-    raise "Unsupported list element type #{element_type}" if element_size.nil?
-    @element_size = T.let(element_size, Integer)
-
-    # Extract number of elements
-    @length = T.let((size_part & 0xFFFF_FFF8) / 8, Integer)
-  end
-
-  sig { returns(Integer) }
-  attr_reader :length
-
-  sig { params(ix: Integer, type: Symbol).returns(Integer) }
-  def get(ix, type) = @segment.read_integer(@data_offset + ix * @element_size, type)
-end
-
-class CapnProto::DataPointer < CapnProto::ListPointer
-  sig { params(segment: CapnProto::Segment, offset: Integer).void }
-  def initialize(segment, offset)
-    super(segment, offset)
-    CapnProto::assert { @element_size == 1 }
+class CapnProto::Data < CapnProto::List
+  sig { params(pointer: CapnProto::Buffer::Reference).void }
+  def initialize(pointer)
+    super(pointer)
+    CapnProto::assert { @element_type == 2 }
   end
 
   sig { returns(String) }
-  def value = @segment.read_string(@data_offset, @length - 1, Encoding::BINARY)
-end
-
-class CapnProto::StringPointer < CapnProto::ListPointer
-  sig { params(segment: CapnProto::Segment, offset: Integer).void }
-  def initialize(segment, offset)
-    super(segment, offset)
-    CapnProto::assert { @element_size == 1 }
-  end
-
-  sig { returns(String) }
-  def value = @segment.read_string(@data_offset, @length - 1, Encoding::UTF_8)
+  def value = @data.read_string(0, @length - 1, Encoding::BINARY)
 end
--- capnproto/struct.rb
+++ capnproto/struct.rb
@@ -106,37 +106,3 @@ class CapnProto::Struct
     klass.new(pointer)
   end
 end
-
-class CapnProto::StructPointer
-  extend T::Sig
-
-  sig { params(segment: CapnProto::Segment, offset: Integer).void }
-  def initialize(segment, offset)
-    @segment = segment
-    @offset = offset
-
-    # Check the type of the pointer
-    offset_part = @segment.read_integer(@offset, :s32)
-    CapnProto::assert { offset_part & 0b11 == 0 }
-
-    # Extract offset of data section
-    offset_from_pointer = (offset_part >> 2) * CapnProto::WORD_SIZE
-    @data_offset = T.let(@offset + offset_from_pointer + CapnProto::WORD_SIZE, Integer)
-
-    # Extract size of data and pointer sections
-    @data_size = T.let(@segment.read_integer(@offset + 4, :u16) * CapnProto::WORD_SIZE, Integer)
-    @pointer_size = T.let(@segment.read_integer(@offset + 6, :u16) * CapnProto::WORD_SIZE, Integer)
-
-    # Calculate offset of pointer section
-    @pointer_offset = T.let(@data_offset + @data_size, Integer)
-  end
-
-  sig { params(offset: Integer, size: Integer, encoding: Encoding).returns(String) }
-  def read_string(offset, size, encoding) = @segment.read_string(@data_offset + offset, size, encoding)
-
-  sig { params(offset: Integer, type: Symbol).returns(Integer) }
-  def read_integer(offset, type) = @segment.read_integer(@data_offset + offset, type)
-
-  sig { params(ix: Integer).returns(Integer) }
-  def pointer_offset(ix) = @pointer_offset + ix * CapnProto::WORD_SIZE
-end
