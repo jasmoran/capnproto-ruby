61ea426489ad1b53ab73b75efb507aca98ddba48
Jack Moran <jack@earth.co.nz>
2024-04-15 19:39:48 +1200

    Catch and format exceptions

--- main.rb
+++ main.rb
@@ -4,13 +4,18 @@
 require 'pp'
 require_relative 'test.capnp'
 
-message = CapnProto::Message.new(CapnProto::Buffer.from_io(STDIN))
+begin
+  message = CapnProto::Message.new(CapnProto::Buffer.from_io(STDIN))
 
-root = message.root
-exit if root.nil?
+  root = message.root
+  exit if root.nil?
 
-decoded = CapnProto::Struct.get_pointer_references(root)
-exit if decoded.nil?
+  decoded = CapnProto::Struct.get_pointer_references(root)
+  exit if decoded.nil?
 
-person = Test::Person.new(message, decoded[0], decoded[1])
-pp person.to_h
+  person = Test::Person.new(message, decoded[0], decoded[1])
+  pp person.to_h
+rescue => e
+  STDERR.puts("#{e.class}: #{e.message}")
+  e.backtrace.to_a.reject { |line| line.include?('/gems/sorbet-runtime-') }.each { |line| STDERR.puts(line) }
+end
