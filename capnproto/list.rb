edc29cd934d595798cfe2bc29ebb94a137050980
Jack Moran <jack@earth.co.nz>
2024-04-19 10:14:48 +1200

    Add far pointer support to Lists

--- capnproto/list.rb
+++ capnproto/list.rb
@@ -27,52 +27,58 @@ class CapnProto::List
 
   public
 
-  sig { params(pointer: CapnProto::Reference).returns(T.nilable(T.attached_class)) }
-  def self.from_pointer(pointer)
-    pointer_value = pointer.read_integer(0, false, 64)
-    return if pointer_value.zero?
+  sig { params(pointer_ref: CapnProto::Reference).returns(T.nilable(T.attached_class)) }
+  def self.from_pointer(pointer_ref)
+    # Process far pointers
+    pointer_ref, content_ref = pointer_ref.dereference_pointer
 
-    # Check the type of the pointer
-    offset_part = pointer.read_integer(0, true, 32)
+    # Grab lower 32 bits as offset and upper 32 bits as size
+    pointer_data = pointer_ref.read_string(0, CapnProto::WORD_SIZE, Encoding::BINARY)
+    offset_part, size_part = T.cast(pointer_data.unpack('l<L<'), [Integer, Integer])
+
+    # Check for NULL pointer
+    return nil if offset_part.zero? && size_part.zero?
+
+    # Check this is a list pointer
     pointer_type = offset_part & 0b11
     CapnProto::assert("List pointer has type #{pointer_type}") { pointer_type == 1 }
 
-    # Extract offset of data section
-    offset_from_pointer = (offset_part >> 2) * CapnProto::WORD_SIZE
-    data_offset = offset_from_pointer + CapnProto::WORD_SIZE
-
-    # Extract type of list elements
-    size_part = pointer.read_integer(4, false, 32)
-    element_type = size_part & 0b111
-
     # Determine the length of the list
-    list_size = size_part >> 3
-    length = list_size
-
-    # Fetch tag for composite type elements
-    data_words = 0
-    pointer_words = 0
-    if element_type == 7
-      tag_pointer = pointer.apply_offset(data_offset, CapnProto::WORD_SIZE)
-      length, data_words, pointer_words = CapnProto::Struct.decode_pointer(tag_pointer)
-      data_offset += CapnProto::WORD_SIZE
-    end
+    length = size_part >> 3
 
     # Determine the size of the data section
+    element_type = size_part & 0b111
     data_size = case element_type
       # Void type elements
       when 0 then 0
       # Bit type elements
-      when 1 then (list_size + 7) / 8
+      when 1 then (length + 7) / 8
       # Integer type elements
-      when 2, 3, 4, 5 then list_size << (element_type - 2)
-      # Pointer and Composite type elements
-      else list_size * CapnProto::WORD_SIZE
+      when 2, 3, 4, 5 then length << (element_type - 2)
+      # Pointer type elements
+      when 6 then length * CapnProto::WORD_SIZE
+      # Composite type elements
+      else (length + 1) * CapnProto::WORD_SIZE
     end
 
-    data = pointer.apply_offset(data_offset, data_size)
+    # Extract data section
+    if content_ref.nil?
+      data_offset = ((offset_part >> 2) + 1) * CapnProto::WORD_SIZE
+      data_ref = pointer_ref.apply_offset(data_offset, data_size)
+    else
+      data_ref = content_ref.apply_offset(0, data_size)
+    end
 
-    self.new(data, length, element_type, data_words, pointer_words)
+    # Fetch tag for composite type elements
+    data_words = 0
+    pointers_words = 0
+    if element_type == 7
+      # Decode tag as a struct pointer
+      length, data_words, pointers_words = CapnProto::Struct.decode_pointer(data_ref)
+      data_ref = data_ref.apply_offset(CapnProto::WORD_SIZE, data_size - CapnProto::WORD_SIZE)
+    end
+
+    self.new(data_ref, length, element_type, data_words, pointers_words)
   end
 
   sig { returns(Integer) }
