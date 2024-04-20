f97b702f53676bfa8e409e6e9e86caef9820c71c
Jack Moran <jack@earth.co.nz>
2024-04-20 12:07:31 +1200

    schema: Decode requestedFiles & imports

--- schema.capnp.rb
+++ schema.capnp.rb
@@ -26,10 +26,13 @@ module Schema
     sig { returns(T.nilable(CapnpVersion)) }
     def capnpVersion = CapnpVersion.from_pointer(read_pointer(2))
 
+    sig { returns(T.nilable(CapnProto::StructList[RequestedFile])) }
+    def requestedFiles = RequestedFile::List.from_pointer(read_pointer(1))
+
     sig { returns(T::Hash[Symbol, T.untyped]) }
     def to_h = {
       capnpVersion: capnpVersion.to_h,
-      requestedFiles: [],
+      requestedFiles: requestedFiles&.map(&:to_h),
     }
 
     class RequestedFile < CapnProto::Struct
@@ -39,11 +42,14 @@ module Schema
       sig { returns(T.nilable(CapnProto::String)) }
       def filename = CapnProto::String.from_pointer(read_pointer(0))
 
+      sig { returns(T.nilable(CapnProto::StructList[Import])) }
+      def imports = Import::List.from_pointer(read_pointer(1))
+
       sig { returns(T::Hash[Symbol, T.untyped]) }
       def to_h = {
         id: id,
         filename: filename&.value,
-        imports: nil,
+        imports: imports&.map(&:to_h),
       }
 
       class Import < CapnProto::Struct
@@ -58,6 +64,20 @@ module Schema
           id: id,
           name: name&.value,
         }
+
+        class List < CapnProto::StructList
+          Elem = type_member {{fixed: Import}}
+
+          sig { override.returns(T.class_of(Import)) }
+          def element_class = Import
+        end
+      end
+
+      class List < CapnProto::StructList
+        Elem = type_member {{fixed: RequestedFile}}
+
+        sig { override.returns(T.class_of(RequestedFile)) }
+        def element_class = RequestedFile
       end
     end
   end
