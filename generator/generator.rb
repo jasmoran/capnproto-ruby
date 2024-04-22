32716ca419e91afbf8331455dc0c7e9b2f1b8079
Jack Moran <jack@earth.co.nz>
2024-04-22 20:17:25 +1200

    Begin processing fields

--- generator/generator.rb
+++ generator/generator.rb
@@ -67,14 +67,39 @@ class CapnProto::Generator
 
   sig { params(node: Schema::Node).void }
   def self.process_struct(node)
-    warn 'Ignoring nodes nested in struct' unless node.nestedNodes&.length&.nonzero?
+    warn 'Ignoring nodes nested in struct' unless node.nestedNodes&.length.to_i.zero?
     raise 'Generic structs are not supported' if node.isGeneric
 
     fields = node.struct.fields
     raise 'No fields found' if fields.nil?
 
     fields.sort_by(&:codeOrder).each do |field|
-      pp field.to_h
+      pp process_field(field)
+    end
+  end
+
+  sig { params(field: Schema::Field).returns(T::Array[String]) }
+  def self.process_field(field)
+    raise 'Groups not supported' if field.which == Schema::Field::Which::Group
+    warn 'Ignoring annotations' unless field.annotations&.length.to_i.zero?
+    raise 'Unions not supported' if field.discriminantValue != 0xFFFF
+    name = field.name
+    raise 'Field without a name' if name.nil?
+    type = field.slot.type
+    raise 'Field without a type' if type.nil?
+    case type.which
+    when Schema::Type::Which::Void
+      ['sig { void }', "def #{name.value}; end"]
+    when Schema::Type::Which::Bool
+      default_value = field.slot.defaultValue&.bool ? '0xFF' : '0x00'
+      offset = field.slot.offset / 8
+      mask = (1 << (field.slot.offset % 8)).to_s(16)
+      [
+        'sig { returns(T::Boolean) }',
+        "def #{name.value} = (read_integer(#{offset}, false, 8, #{default_value}) & 0x#{mask}) != 0"
+      ]
+    else
+      []
     end
   end
 end
