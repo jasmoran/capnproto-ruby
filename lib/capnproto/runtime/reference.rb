1857737d74e454ba8c548a592e5c5698288df34b
Jack Moran <jack@earth.co.nz>
2024-05-15 08:56:06 +1200

    References now apply to segments not buffers

--- lib/capnproto/runtime/reference.rb
+++ lib/capnproto/runtime/reference.rb
@@ -7,19 +7,25 @@ require_relative "buffer/buffer"
 class CapnProto::Reference
   extend T::Sig
 
-  sig { params(buffer: CapnProto::Buffer, position: Integer).void }
-  def initialize(buffer, position)
-    @buffer = buffer
+  sig { params(segment: CapnProto::Segment, position: Integer).void }
+  def initialize(segment, position)
+    @segment = segment
     @position = position
   end
 
   EMPTY = T.let(
-    CapnProto::Reference.new(CapnProto::IOBuffer.new(IO::Buffer.for("")).freeze, 0).freeze,
+    CapnProto::Reference.new(
+      CapnProto::FlatMessage.new(CapnProto::StringBuffer.new("")).segment(0),
+      0
+    ),
     CapnProto::Reference
   )
 
   NULL_POINTER = T.let(
-    CapnProto::Reference.new(CapnProto::IOBuffer.new(IO::Buffer.for("\x00\x00\x00\x00\x00\x00\x00\x00")).freeze, 0).freeze,
+    CapnProto::Reference.new(
+      CapnProto::FlatMessage.new(CapnProto::StringBuffer.new("\x00\x00\x00\x00\x00\x00\x00\x00")).segment(0),
+      0
+    ),
     CapnProto::Reference
   )
 
@@ -27,68 +33,68 @@ class CapnProto::Reference
   attr_reader :position
 
   sig { overridable.params(offset: Integer).returns(CapnProto::Reference) }
-  def offset_position(offset) = self.class.new(@buffer, @position + offset)
+  def offset_position(offset) = self.class.new(@segment, @position + offset)
 
   sig { params(offset: Integer, length: Integer).returns(String) }
   def read_string(offset, length)
-    @buffer.read_string(@position + offset, length)
+    @segment.read_string(@position + offset, length)
   end
 
   sig { params(offset: Integer, length: Integer).returns(String) }
   def read_bytes(offset, length)
-    @buffer.read_bytes(@position + offset, length)
+    @segment.read_bytes(@position + offset, length)
   end
 
   sig { params(offset: Integer).returns(Integer) }
   def read_u8(offset)
-    @buffer.read_u8(@position + offset)
+    @segment.read_u8(@position + offset)
   end
 
   sig { params(offset: Integer).returns(Integer) }
   def read_u16(offset)
-    @buffer.read_u16(@position + offset)
+    @segment.read_u16(@position + offset)
   end
 
   sig { params(offset: Integer).returns(Integer) }
   def read_u32(offset)
-    @buffer.read_u32(@position + offset)
+    @segment.read_u32(@position + offset)
   end
 
   sig { params(offset: Integer).returns(Integer) }
   def read_u64(offset)
-    @buffer.read_u64(@position + offset)
+    @segment.read_u64(@position + offset)
   end
 
   sig { params(offset: Integer).returns(Integer) }
   def read_s8(offset)
-    @buffer.read_s8(@position + offset)
+    @segment.read_s8(@position + offset)
   end
 
   sig { params(offset: Integer).returns(Integer) }
   def read_s16(offset)
-    @buffer.read_s16(@position + offset)
+    @segment.read_s16(@position + offset)
   end
 
   sig { params(offset: Integer).returns(Integer) }
   def read_s32(offset)
-    @buffer.read_s32(@position + offset)
+    @segment.read_s32(@position + offset)
   end
 
   sig { params(offset: Integer).returns(Integer) }
   def read_s64(offset)
-    @buffer.read_s64(@position + offset)
+    @segment.read_s64(@position + offset)
   end
 
   sig { params(offset: Integer).returns(Float) }
   def read_f32(offset)
-    @buffer.read_f32(@position + offset)
+    @segment.read_f32(@position + offset)
   end
 
   sig { params(offset: Integer).returns(Float) }
   def read_f64(offset)
-    @buffer.read_f64(@position + offset)
+    @segment.read_f64(@position + offset)
   end
 
   sig { returns([CapnProto::Reference, T.nilable(CapnProto::Reference)]) }
-  def dereference_pointer = @buffer.dereference_pointer(self)
+  def dereference_pointer = @segment.dereference_pointer(self)
 end
