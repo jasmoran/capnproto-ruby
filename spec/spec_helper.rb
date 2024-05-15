67964983114709b5fc823c44bbbe46bb7b4128f0
Jack Moran <jack@earth.co.nz>
2024-05-15 12:51:53 +1200

    Collect coverage when running tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,43 @@
-# typed: strong
+# typed: ignore
 # frozen_string_literal: true
 
-require "capnproto"
-require "minitest/autorun"
+require "simplecov"
+require "simplecov-lcov"
 
+# Clear out old coverage results
+coverage_dir = File.join(__dir__, "..", "coverage")
+FileUtils.rm_rf(coverage_dir) if File.exist?(coverage_dir)
+
+# Collect test coverage information
+SimpleCov.start do
+  # Output LCOV format for CI
+  if ENV["CI"]
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = "coverage/lcov.info"
+    end
+
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+
+  # Enable branch coverage
+  enable_coverage :branch
+
+  # Filter out spec files and generated capnp.rb files
+  add_filter %r{^/spec/}
+  add_filter ".capnp.rb"
+end
+
+require "capnproto"
+require "capnproto/generator"
+require "minitest"
+require "minitest/spec"
+require "minitest/mock"
+
+# Enable checking for sigs marked checked tests
+T::Configuration.enable_checking_for_sigs_marked_checked_tests
+
+# Alias describe to context for clarity in specs
 module Minitest
   class Spec
     module DSL
@@ -11,3 +45,6 @@ module Minitest
     end
   end
 end
+
+# Run tests
+Minitest.autorun
